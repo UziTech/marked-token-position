f60e251713d125fd3ec26539bcc26b63307657dc
Tony Brix <tony@brix.ninja>
2025-07-19 15:26:58 -0600

    fix type tests

--- spec/index.test-types.ts
+++ spec/index.test-types.ts
@@ -1,12 +1,12 @@
 import { Marked, type Token } from 'marked';
-import markedTokenPosition, { setPositions, type TokenWithPosition } from 'marked-token-position';
+import markedTokenPosition, { addTokenPositions, type TokenWithPosition } from 'marked-token-position';
 
 const marked = new Marked();
 
 marked.use(markedTokenPosition());
 
 const tokens: Token[] = marked.lexer('example markdown');
-const tokensWithPosition: TokenWithPosition[] = setPositions(tokens, 'example markdown');
+const tokensWithPosition: TokenWithPosition[] = addTokenPositions(tokens);
 console.log(tokensWithPosition[0]?.position.offset);
 console.log(tokensWithPosition[0]?.position.line);
 console.log(tokensWithPosition[0]?.position.column);

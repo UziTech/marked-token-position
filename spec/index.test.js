f56fb1ee9ff78968d26f0250cc17656c018e02e0
Tony Brix <tony@brix.ninja>
2025-07-21 22:55:05 -0600

    feat: add original markdown to fix reflink

--- spec/index.test.js
+++ spec/index.test.js
@@ -1,11 +1,34 @@
 import { describe, test } from 'node:test';
-import { Marked } from 'marked';
+import { Marked, walkTokens } from 'marked';
 import markedTokenPosition, { addTokenPositions } from '../src/index.ts';
 import { readFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
 
 function getFixture(path) {
   return readFile(resolve(import.meta.dirname, 'fixtures', path), { encoding: 'utf-8' });
+}
+
+function getLineColumn(offset, markdown) {
+  const lines = markdown.substring(0, offset).split('\n');
+
+  return {
+    line: lines.length - 1,
+    column: lines.at(-1)?.length ?? 0,
+  };
+}
+
+function checkPositions(t, tokens, markdown) {
+  walkTokens(tokens, (token) => {
+    const offsetString = markdown.substring(token.position.start.offset, token.position.end.offset);
+    const { line: startLine, column: startColumn } = getLineColumn(token.position.start.offset, markdown);
+    const { line: endLine, column: endColumn } = getLineColumn(token.position.end.offset, markdown);
+
+    t.assert.equal(offsetString, token.raw);
+    t.assert.equal(startLine, token.position.start.line);
+    t.assert.equal(startColumn, token.position.start.column);
+    t.assert.equal(endLine, token.position.end.line);
+    t.assert.equal(endColumn, token.position.end.column);
+  });
 }
 
 function testPosition(t) {
@@ -89,12 +112,15 @@ describe('addTokenPositions', () => {
     const md = '# example markdown';
     const tokens = marked.lexer(md);
     t.assert.snapshot(addTokenPositions(tokens));
+    checkPositions(t, tokens, md);
   });
 
   test('reference', async(t) => {
     const marked = new Marked();
     const md = await getFixture('reference.md');
     const tokens = marked.lexer(md);
-    t.assert.snapshot(addTokenPositions(tokens));
+    t.assert.snapshot(addTokenPositions(tokens, md));
+    // TODO: enable this when it is passing
+    // checkPositions(t, tokens, md);
   });
 });
